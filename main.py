edcf24ae586bd65d21673ae37bf9ff5ee2820379
Gabriel <bielcarvalho@outlook.com.br>
2020-06-18 15:56:54 -0300

    Adds file storage
    Argparse implementation not working

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import argparse
+from os import path
 from mdp import MDP
 
 states_idx = dict()
@@ -14,64 +16,59 @@ actions = {-1: 0,
            3: '>'}
 
 mdp_problem = MDP()
-f = open('G:\\Meu Drive\\Study\\SI\\2020.1\\Planejamento\\EP2\\TestesGrid\\FixedGoalInitialState\\navigation_1.net')
+
+
+# input_file = open('G:\\Meu Drive\\Study\\SI\\2020.1\\Planejamento\\EP2\\TestesGrid\\FixedGoalInitialState
+# \\navigation_1.net')
 
 
 def read_states():
     global states_name, states_idx, mdp_problem
     states_name = []
-    line = f.readline().strip()
+    line = input_file.readline().strip()
     while not line.startswith('end'):
         states_name.extend([name for name in line.split(', ')])
-        line = f.readline().strip()
+        line = input_file.readline().strip()
 
-    # print(states_name)
     for i, state in enumerate(states_name):
         states_idx[state] = i
 
     mdp_problem.set_state_num(len(states_idx))
 
 
-# def read_line():
-#     line = f.readline().strip()
-#     if not line.startswith('end'):
-#         return line
-#     return ''
-
 def read_action(name):
     global mdp_problem
     action_id = actions_name[name]
-    line = f.readline().strip()
+    line = input_file.readline().strip()
     while not line.startswith('end'):
         src, dest, prob = line.split()[:3]
         mdp_problem.add_action(states_idx[src], states_idx[dest], action_id, float(prob))
-        line = f.readline().strip()
+        line = input_file.readline().strip()
 
 
 def read_cost():
     global mdp_problem
-    line = f.readline().strip()
+    line = input_file.readline().strip()
     while not line.startswith('end'):
         src, action, cost = line.split()
         mdp_problem.add_cost(states_idx[src], actions_name[action], float(cost))
-        line = f.readline().strip()
+        line = input_file.readline().strip()
 
 
 def read_state(initial=True):
-    state = states_idx[f.readline().strip()]
+    state = states_idx[input_file.readline().strip()]
     # print(state)
     if initial:
         mdp_problem.set_initial_state(state)
     else:
         mdp_problem.set_goal(state)
-    f.readline()
+    input_file.readline()
 
 
 def read_input():
     try:
         while True:
-            line = f.readline().strip()
-            print(line)
+            line = input_file.readline().strip()
             if line.startswith('states'):
                 read_states()
             elif line.startswith('action'):
@@ -96,7 +93,11 @@ def get_state_pos(name: str):
     return int(p[1]) - 1, int(p[0]) - 1
 
 
-def grid_res(policy):
+def to_str(old):
+    return str(old).replace('.', ',')
+
+
+def print_res(policy, folder_name, file_number, iteration):
     height, width = get_state_pos(states_name[-1])
     grid = [[0 for _ in range(width + 1)] for _ in range(height + 1)]
     for idx, action in enumerate(policy):
@@ -104,24 +105,56 @@ def grid_res(policy):
         grid[x][y] = actions[action]
 
     # print(grid)
-    return grid[::-1]
+    grid = grid[::-1]
+
+    output_file.write(f'{iteration}\n')
+
+    for key, val in mdp_problem.get_stats().items():
+        output_file.write(f'{key}: {val}\n')
+
+    # output_file.write(f'{mdp_problem.get_stats()["Iterations"]} {mdp_problem.get_stats()["Run_Time"]} \n')
+    for line in grid:
+        output_file.write(' '.join(str(item) for item in line) + '\n')
+        # print(' '.join(str(item) for item in line))
+
+    stats = mdp_problem.get_stats()
+    stats['Iteration'] = iteration
+    stats['Folder'] = folder_name
+    stats['File'] = file_number
+    # for key, val in stats:
+    stats_file.write(';'.join(to_str(val) for val in stats.values()) + '\n')
 
 
-def print_grid(grid):
-    for line in (grid):
-        # print(line)
-        print(' '.join(str(item) for item in line))
+def solve(folder_name, file_number):
+    read_input()
+    print_res(mdp_problem.value_iteration(), folder_name, file_number, 'Value Iteration')
+    print_res(mdp_problem.policy_iteration(), folder_name, file_number, 'Policy Iteration')
 
 
-read_input()
+# TODO: Fix args receiving
 
-# mdp_problem.value_iteration()
-# grid = grid_res(mdp_problem.get_policy())
-# print_grid(grid)
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input_file",
+                default='C:\\Users\\Gabriel\\Documents\\Grid\\FixedGoalInitialState\\navigation_1.net',
+                help=f"arquivo de entrada para leitura")
+ap.add_argument("-o", "--output_file",
+                help=f"arquivo de saida para salvar grids")
+ap.add_argument("-st", "--stats_file", default='stats.csv',
+                help=f"arquivo para armazenar estatisticas que serao usadas para gerar graficos")
+# ap.add_argument("-s", "--silent", default=False, action='store_true',
+#                 help=f"executar sem impressoes para tela")
+args = vars(ap.parse_args())
 
-mdp_problem.policy_iteration()
-grid = grid_res(mdp_problem.get_policy())
-print_grid(grid)
+input_file = open(args['input_file'])
+# if 'output_file' in args:
+#     output_file = args['output_file']
+# else:
+#     output_file = path.join(path.dirname(__file__), path.basename(args['input_file']).rsplit('.', 1)[0] + '.txt')
 
-# TODO: Implement file receiving through args with argparse
-# TODO: Implement storage for output in files
+# output_file = open(output_file, 'w')
+output_file = open(path.join(path.dirname(__file__), 'output.txt'), 'w')
+stats_file = open(path.join(path.dirname(__file__), args['stats_file']), 'w')
+
+solve(path.basename(path.dirname(args['input_file'])), args['input_file'].rsplit('.', 1)[0].rsplit('_', 1)[1])
+stats_file.close()
+output_file.close()
--- mdp.py
+++ mdp.py
@@ -1,6 +1,4 @@
 import time
-from copy import deepcopy
-
 import numpy as np
 
 
@@ -11,15 +9,8 @@ class MDP:
         self.epsilon = epsilon
         self.iter = self.initial_state = self.goal = self.run_time = -1
 
-        # self.successors = [[list for _ in range(4)] for _ in range(self.states)]
-        # self.probabilities = [[list for _ in range(4)] for _ in range(self.states)]
-
-        # self.successors = np.array(np.full((self.states, 4), np.nan), dtype=int)
-        # self.probabilities = np.array(np.full((self.states, 4), np.nan), dtype=float)
         if num_states != 0:
             self.set_state_num(num_states)
-            # self.transitions = [[[] for _ in range(4)] for _ in range(self.states)]
-            # self.costs = [[1.0 for _ in range(4)] for _ in range(self.states)]
 
         else:
             self.states = num_states
@@ -33,18 +24,10 @@ class MDP:
         self.costs = [[1.0 for _ in range(4)] for _ in range(self.states)]
 
     def add_action(self, source: int, destination: int, action_idx: int, probability: float):
-        # self.transitions[source][action_idx] = (destination, probability, cost)
-        # self.transitions[source][action_idx][0] = destination
-        # self.transitions[source][action_idx][1] = probability
         self.transitions[source][action_idx].append((destination, probability))
-        # self.probabilities[source][action_idx] = probability
 
     def add_cost(self, source: int, action_idx: int, cost: float):
         self.costs[source][action_idx] = cost
-
-    def get_actions(self, state: int):
-        # return [action for action in self.transitions[state] if action is not None]
-        pass
 
     def set_goal(self, new_goal: int):
         self.goal: int = new_goal
@@ -55,12 +38,11 @@ class MDP:
     def _bellman_backup(self, state: int):
         if state == self.goal:
             return -1, 0
+
         Q_s = np.full(4, np.nan)
-        # for i in range(4):
-        # np.where(self.successors != np.nan, 1+(self.V[self.successors]))
-        # Q = np.array(self.successors)
+
         for action in range(4):
-            if len(self.transitions[state][action]) > 0:
+            if self.transitions[state][action]:
                 Q_s[action] = self.costs[state][action]
                 for successor, probability in self.transitions[state][action]:
                     Q_s[action] += self.V[successor] * probability
@@ -68,7 +50,6 @@ class MDP:
         return int(np.argmin(Q_s)), float(np.min(Q_s))
 
     def value_iteration(self):
-        # self.V = [[1 for _ in range(self.width)] for _ in range(self.height)]
         start_time = time.time()
         self.V = np.ones(self.states)
         self.V[self.goal] = 0
@@ -80,22 +61,19 @@ class MDP:
             max_res = 0
             new_V = self.V.copy()
             self.iter += 1
-            # print('Iteracao ', self.iter)
-            # print('Residual ', max_res)
 
             for state in range(self.states):
                 new_V[state] = self._bellman_backup(state)[1]
-                # print(new_V[state])
                 max_res = max(max_res, abs(new_V[state] - self.V[state]))
-            # new_V[self.goal] = 0
 
             # max_res = np.max(np.abs(new_V-self.V))
             self.V = new_V
-        print(self.V)
+
         self.policy = [self._bellman_backup(state)[0] for state in range(self.states)]
         self.run_time = time.time() - start_time
+        return self.policy
 
-    def _proper_actions(self, state):
+    def _get_proper_action(self, state):
         for idx, action in enumerate(self.transitions[state]):
             if len(action) >= 1:
                 for successors in action:
@@ -110,14 +88,8 @@ class MDP:
         self.V[self.goal] = 0
         self.iter = 0
 
-        # self.policy = [next((idx for idx, action in enumerate(self.transitions[state]) if action and
-        #                      action[successors][0] != state for successors in action), 0)
-        #                for state in range(self.states)]
-
-        self.policy = [self._proper_actions(state) for state in range(self.states)]
+        self.policy = [self._get_proper_action(state) for state in range(self.states)]
         self.policy[self.goal] = -1
-        # for state in range(self.states):
-        #     self.V[state] = self._bellman_backup(state)[1]
 
         # self.policy = [self._bellman_backup(state)[0] for state in range(self.states)]
 
@@ -125,16 +97,11 @@ class MDP:
 
         while old_policy != self.policy:
             self.iter += 1
-            print('Iteracao {}'.format(self.iter))
 
             new_V = self.V.copy()
 
             for state in range(self.states):
                 new_V[state] = self._bellman_backup(state)[1]
-
-            # if new_V == self.V:
-            #     print(new_V)
-            #     exit(1)
 
             self.V = new_V
             # old_policy = deepcopy(self.policy)
@@ -144,12 +111,13 @@ class MDP:
                 self.policy[state] = self._bellman_backup(state)[0]
 
         self.run_time = time.time() - start_time
+        return self.policy
 
     def get_policy(self):
         return self.policy
 
     def get_stats(self):
         return {'Iterations': self.iter,
-                'Run Time:': self.run_time,
-                'Avg Time': self.run_time / self.iter}
+                'Run_Time:': self.run_time,
+                'Avg_Time': self.run_time / self.iter}
 
